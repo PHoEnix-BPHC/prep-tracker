801bbbaca57e07ac415a2e3e2237ea80e79c4377
Sushobhit Srivastava <sushobhitofficial@gmail.com>
2022-06-25 01:52:56 +0530

    Updating Tabs

---
+++ src/Update.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+class Update extends React.Component {
+    render() {
+        return (
+            <div>
+                UPDATING COMPETITION TAB: WILL OPEN BY 11:00 AM 25TH JUNE 2022
+            </div>
+        )
+    }
+}
+
+export default Update
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import 'react-circular-progressbar/dist/styles.css';
 import './index.css';
 import App from './App';
+import Update from "./Update"
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Update />
   </BrowserRouter>
 );
