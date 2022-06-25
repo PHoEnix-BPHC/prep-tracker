90cc755c1abfb0c8ef867fc99225b30e3e0356a6
Sushobhit Srivastava <sushobhitofficial@gmail.com>
2022-06-25 12:44:59 +0530

    Fixed encryption error

--- src/Components/Encryption.js
+++ src/Components/Encryption.js
@@ -1,7 +1,7 @@
-import { EncryptStorage } from "encrypt-storage";
+// import { EncryptStorage } from "encrypt-storage";
 
 
-export const localStorage = new localStorage("a8e309mchiaehbiusdhfsbdfsdf", {
-    storageType: "localStorage",
-    encAlgorithm: "Rabbit"
-})
+// export const localStorage = new localStorage("a8e309mchiaehbiusdhfsbdfsdf", {
+//     storageType: "localStorage",
+//     encAlgorithm: "Rabbit"
+// })
